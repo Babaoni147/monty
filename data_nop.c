875b21069ecc07937f2f9c25af9760bc023c16e8
Babaoni147 <obahedwin@ymail.com>
2024-03-29 03:53:37 +0100

    myfiles

---
+++ data_nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * data_nop- function that cast type nothing
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void data_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
